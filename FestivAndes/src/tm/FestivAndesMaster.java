f77c9d772abcc23eae1eb5160cb402e13d08445c
Sebastian <js.millan10@uniandes.edu.co>
2017-04-12 21:01:01 -0500

    Todo Sebastian

--- FestivAndes/src/tm/FestivAndesMaster.java
+++ FestivAndes/src/tm/FestivAndesMaster.java
@@ -369,7 +369,7 @@ public class FestivAndesMaster {
 			daoTablaCompañias.setConn(conn);
 			compañias = daoTablaCompañias.darInfoCompaniasId(idE);
 			List<Espectaculo> listaEsp = compañias.getEspectaculos().getEspectaculos();
-			ArrayList<Funcion> funcionesEspect = listaEsp.get(0).getFunciones();
+			ArrayList<Funcion> funcionesEspect = listaEsp.get(1).getFunciones();
 		} catch (SQLException e) {
 			System.err.println("SQLException:" + e.getMessage());
 			e.printStackTrace();
